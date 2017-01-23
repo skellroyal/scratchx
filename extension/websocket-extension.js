009b4472661d3fe9c94e1804afe72e0da902d355
Doris Liao <skellroyal@hotmail.com>
2017-01-23 20:46:16 +0800

    Add files via upload

---
+++ extension/websocket-extension.js
@@ -0,0 +1,31 @@
+new (function() {
+    var ext_ = this;
+
+    // Extension name
+    var name = 'WebSocket extension';
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            ['w', 'connect to %s', 'connect'],
+            ['w', 'disconnect %s', 'disconnect'],
+            [ '', 'send %s to %s', 'send'],
+            ['r', 'message from %s', 'getMessage'],
+            ['r', 'message origin', 'getLastReceivedMessageOrigin'],
+            ['h', 'when data received', 'onMessageReceived'],
+
+            ['r', 'JSON', 'emptyObject'],
+            ['r', 'add string %s:%s to %s', 'addJsonProperty'],
+            ['r', 'add number %s:%n to %s', 'addJsonProperty'],
+            ['r', 'get property:%s from JSON:%s', 'getJsonProperty'],
+        ]
+    };
+
+    var scriptpath = document.currentScript.src.match(/.*\//);
+    $.getScript(scriptpath + 'ws-ext.js')
+        .done( function(ws_ext, textStatus) {
+            var eventTarget = document.createDocumentFragment();
+            ws_ext_init(ext_, eventTarget);
+            ScratchExtensions.register(name, descriptor, ext_);
+        });
+
+})();
