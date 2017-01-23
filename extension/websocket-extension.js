8b671c866118a5d555aa95270325873cb1ba91b7
Doris Liao <skellroyal@hotmail.com>
2017-01-23 20:49:30 +0800

    Update websocket-extension.js

--- extension/websocket-extension.js
+++ extension/websocket-extension.js
@@ -8,15 +8,6 @@ new (function() {
         blocks: [
             ['w', 'connect to %s', 'connect'],
             ['w', 'disconnect %s', 'disconnect'],
-            /*[ '', 'send %s to %s', 'send'],
-            ['r', 'message from %s', 'getMessage'],
-            ['r', 'message origin', 'getLastReceivedMessageOrigin'],
-            ['h', 'when data received', 'onMessageReceived'],
-
-            ['r', 'JSON', 'emptyObject'],
-            ['r', 'add string %s:%s to %s', 'addJsonProperty'],
-            ['r', 'add number %s:%n to %s', 'addJsonProperty'],
-            ['r', 'get property:%s from JSON:%s', 'getJsonProperty'],*/
         ]
     };
 
